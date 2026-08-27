8bc4cba46b994df7b726eb06f3a0aede581dab76
lucchettan <lucchettan@gmail.com>
2026-08-27 12:30:45 +0200

    Clean the code

--- tests/RefactoringExercise/RefactoringExercise/UserRepository.swift
+++ tests/RefactoringExercise/RefactoringExercise/UserRepository.swift
@@ -11,22 +11,22 @@ protocol UserRepository: Sendable {
      func fetchUsers() async throws -> [User]
  }
 
- struct RemoteUserRepository: UserRepository {
-     private let session: URLSession
-     private let url: URL
-
-     init(session: URLSession = .shared,
-          url: URL = URL(string: "https://api.example.com/users")!) {
-         self.session = session
-         self.url = url
-     }
-
-     func fetchUsers() async throws -> [User] {
-         let (data, response) = try await session.data(from: url)
-         guard let http = response as? HTTPURLResponse,
-               (200..<300).contains(http.statusCode) else {
-             throw URLError(.badServerResponse)
-         }
-         return try JSONDecoder().decode([User].self, from: data)
-     }
- }
+struct RemoteUserRepository: UserRepository {
+    private let session: URLSession
+    private let url: URL
+    
+    init(session: URLSession = .shared,
+         url: URL = URL(string: "https://api.example.com/users")!) {
+        self.session = session
+        self.url = url
+    }
+    
+    func fetchUsers() async throws -> [User] {
+        let (data, response) = try await session.data(from: url)
+        guard let http = response as? HTTPURLResponse,
+              (200..<300).contains(http.statusCode) else {
+            throw URLError(.badServerResponse)
+        }
+        return try JSONDecoder().decode([User].self, from: data)
+    }
+}
